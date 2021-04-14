4fd8d953c4ec31e11bc54011029e2c40bf5514de
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-14 09:36:32 -0400

    Added firestore to save users on database after sign in with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,6 +4,30 @@ import 'firebase/auth';
 //importing firebase api key
 import Config from './Api_Key';
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(Config);
 
 export const auth = firebase.auth();
