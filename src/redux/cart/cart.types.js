ef93e25eac851f3f079719628bda424109ee5b02
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-21 11:15:19 -0400

    Added cart.types to avoid typos into cart reducer and cart actions files

---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,6 @@
+//We use this to avoid typos inside the reducer and action files, this modularized the code
+const CartActionTypes = {
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+};
+
+export default CartActionTypes;
