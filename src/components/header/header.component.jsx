9adc82df672697c28886a60e31fe1a2ee571ab78
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-21 10:44:30 -0400

    Added cart-icon component and initialized cart-dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './header.styles.scss';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import './header.styles.scss';
 
 
 const Header = ({ currentUser }) => (
@@ -23,7 +27,9 @@ const Header = ({ currentUser }) => (
                 <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 );
 
