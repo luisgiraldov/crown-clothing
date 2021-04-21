164d114dd2636314a787cb9ee1526aebd89fbb26
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-21 11:36:19 -0400

    Implemented redux state modification to cart component to toogle hidden property

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
-    <div className="cart-icon">
+//we destructure toggleCarthidden from redux that is passed through connect
+const CartIcon = ({ toggleCartHidden }) => (
+    <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
         <span className="item-count">0</span>
     </div>
 );
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+//we pass null as default because we don't need state values from redux, and mapDispatchToProps
+//is giving us the method to modify cart state inside redux (hidden property in this case)
+export default connect(
+    null,
+    mapDispatchToProps)
+    (CartIcon);
