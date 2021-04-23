314753ee58a05949fc3a8396cd25d4daaacfacb5
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-22 23:04:11 -0400

    Added addItem action/reducer to modify cart state inside redux, and implemented it into collectionitem/preview

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 //We use this to avoid typos inside the reducer and action files, this modularized the code
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
 };
 
 export default CartActionTypes;
