d8ce88632e8e22fd8ee7fa23cc3e4f8f88bc96af
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-23 10:38:24 -0400

    Added reduce function to add up quantity property from redux to display itemCount on cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
 //we destructure toggleCarthidden from redux that is passed through connect
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount  }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
 
@@ -18,9 +18,14 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce(
+        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+});
+
 //we pass null as default because we don't need state values from redux, and mapDispatchToProps
 //is giving us the method to modify cart state inside redux (hidden property in this case)
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps)
     (CartIcon);
