e9a4be7d86d9ae65dc0dcae0bfcca2525cf1fe66
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-26 17:41:39 -0400

    Added checkout-item component and integrated it into checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import { 
+    selectCartItems, 
+    selectCartTotal 
+} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
@@ -26,8 +31,9 @@ const CheckoutPage = ({ cartItems, total }) => (
             </div>
         </div>
         {
-            cartItems.map(cartItem => 
-                cartItem.name
+            cartItems.map(cartItem => (
+                    <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+                )
             )
         }
 
