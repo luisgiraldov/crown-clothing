621515f011c7a2c2c60d5df5883479ba362d1d6c
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-26 16:50:10 -0400

    Modified checkout button to toggle cart, by doing that it will hide the cart when redirecting to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 //selector
 import { createStructuredSelector } from 'reselect';
@@ -11,19 +12,23 @@ import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
             {
                 cartItems.length ? (
                     cartItems.map(cartItem => (
-                        <CartItem key={CartItem.id} item={cartItem} />
+                        <CartItem key={cartItem.id} item={cartItem} />
                     ))
                 ) :
                 <span className="empty-message">Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>
+        <CustomButton onClick={() => {
+                history.push('/checkout');
+                dispatch(toggleCartHidden());
+            }
+        }>
             GO TO CHECKOUT
         </CustomButton>
     </div>
