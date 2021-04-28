6125cb36057dd587c24f47b36097f6b63b9f69ec
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-27 20:24:41 -0400

    Created a persisted version of the store using redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,17 @@ import { createStore, applyMiddleware } from 'redux';
 //logs the state on the console
 import logger from 'redux-logger';
 
+//persist data on the browser, (example to keep 
+//track of cart items when the page refreshes or the browser is closed)
+import { persistStore } from 'redux-persist';
+
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-export default store;
+//creates a new persisted version of our store
+const persistor = persistStore(store);
+
+export default { store, persistor };
