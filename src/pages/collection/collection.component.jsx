f7395d351c15ad7aab3ef80c2b0d8f9bf2a10201
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-29 21:29:02 -0400

    Added collection-item component into collection component, and included styles

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,11 +8,19 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => (
-    <div className="collection">
-        <h2>COLLECTION PAGE</h2>
-    </div>
-);
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return (
+        <div className="collection-page">
+            <h2 className='title'>{ title }</h2>
+            <div className="items">
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item} />)
+                }
+            </div>
+        </div>
+    );
+} 
 
 //The ownProps gives us all the props that has been passed to the CollectionPage component
 //match is being passed through shop component (Route from react-router-dom)
